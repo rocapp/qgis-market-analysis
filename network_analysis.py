f437eeeeeebf10e67bd7607aa9dc499b06f1d73d
Robert Capps <rocapp@gmail.com>
2016-08-18 14:18:18 -0400

    mac changes

--- network_analysis.py
+++ network_analysis.py
@@ -41,34 +41,21 @@ vl2_ = QgsMapCanvasLayer(vl2)
 vl2_.setVisible(True)
 canvas.refresh()
 
+vl3 = QgsVectorLayer()
+vl3_ = QgsMapCanvasLayer(vl3)
+vl3_.setVisible(True)
+canvas.refresh()
+
 canvas.setExtent(vl2.extent())
-# clayers = list(map(lambda clay: QgsMapCanvasLayer(clay), 
-#             QgsMapLayerRegistry.instance().mapLayers().values()))
-canvas.setLayerSet([vl_, vl2_])
+canvas.setLayerSet([vl2_, vl3_])
 
 mapRenderer = QgsMapRenderer()
-# mapRenderer.setDestinationCrs(crs)
 mapRenderer.setProjectionsEnabled(True)
-
-c = QgsComposition(mapRenderer)
-c.setPlotStyle(QgsComposition.Print)
-
-dpi = c.printResolution()
-dpmm = dpi / 25.4
-width = int(dpmm * c.paperWidth())
-height = int(dpmm * c.paperHeight())
-
-composerMap = QgsComposerMap(c, 0, 0, c.paperWidth(), c.paperHeight())
-composerMap.hasFrame()
-c.addItem(composerMap)
 
 img = QImage(QSize(width, height), QImage.Format_ARGB32)
 color = QColor(255, 255, 255)
 img.fill(color.rgb())
 
-# img_painter = QPainter()
-# img_painter.begin(img)
-# img_painter.setRenderHint(QPainter.Antialiasing)
 canvas.refresh()
 
 ###################################
@@ -82,29 +69,10 @@ def distance():
   builder = QgsGraphBuilder(crs)
 
   pStart = QgsPoint(-83.94216704, 34.53197821)
-  # crsSrc = QgsCoordinateReferenceSystem(4326)
-  # xform = QgsCoordinateTransform(crsSrc, crs)
-  # pStart = xform.transform(pStart)
   print pStart
   delta = canvas.getCoordinateTransform().mapUnitsPerPixel() * 1
 
-  rb1 = QgsRubberBand(canvas)
-  rb1.setIconSize(100)
-  # rb1.setIconType(QgsVertexMarker.ICON_X)
-  # rb1.setPenWidth(50)
-  # rb1.setCenter(QgsPoint(pStart.x(), pStart.y()))
-  rb1.setToGeometry(QgsGeometry.fromPolyline([QgsPoint(pStart.x(), pStart.y())]), None)
-  rb1.setColor(Qt.red)
-  c.addItem(rb1)
-  # canvas.scene().addItem(rb1)
-  if hasattr(vl, "setCacheImage"):
-    vl.setCacheImage(None)
-  vl.triggerRepaint()
-
-  feat = QgsFeature(vl.pendingFields())
-  feat.setGeometry(rb1.asGeometry())
-  (res, outFeats) = vl.dataProvider().addFeatures([feat])
-  vl.updateFields()
+  QgsGeometry.fromPoint(QgsPoint(pStart))
 
   tiedPoints = director.makeGraph(builder, [pStart])
   graph = builder.graph()
@@ -123,64 +91,16 @@ def distance():
         upperBound.append(i)
     i = i + 1
 
+  dataProvider = vl3.dataProvider()
   for i in upperBound:
     centerPoint = graph.vertex(i).point()
-    rb = QgsRubberBand(canvas)
-    rb.setIconSize(10)
-    # rb.setIconType(QgsVertexMarker.ICON_BOX)
-    # rb.setPenWidth(5)
-    rb.setColor(Qt.red)
-    # rb.setCenter(QgsPoint(centerPoint.x(), centerPoint.y()))
-    rb.addPoint(QgsPoint(centerPoint.x() - delta, centerPoint.y() - delta))
-    rb.addPoint(QgsPoint(centerPoint.x() + delta, centerPoint.y() - delta))
-    rb.addPoint(QgsPoint(centerPoint.x() + delta, centerPoint.y() + delta))
-    rb.addPoint(QgsPoint(centerPoint.x() - delta, centerPoint.y() + delta))
-    rb.setToGeometry(QgsGeometry.fromPolyline([QgsPoint(centerPoint.x(), centerPoint.y())]), None)
-    feat = QgsFeature(vl.pendingFields())
-    feat.setGeometry(rb.asGeometry())
-    (res, outFeats) = vl.dataProvider().addFeatures([feat])
-    vl.updateFields()
-    c.addItem(rb)
+    newPt = QgsGeometry.fromPoint(centerPoint)
+    feature = QgsFeature()
+    feature.setGeometry(newPt)
+    dataProvider.addFeatures([feature])
   canvas.refresh()
-  c.refreshItems()
   canvas.zoomToFullExtent()
   canvas.refresh()
   canvas.saveAsImage("Render2.png")
-  # def exportMap():
-  #   vl_.setVisible(False)
-  #   canvas.refresh()
-  #   canvas.setLayerSet([vl_, vl2_])
-  #   canvas.saveAsImage("Render1.png")
-  #   qgs.exitQgis()
-  #   qgs.exit()
-  # QTimer.singleShot(1000, exportMap)
-  # writer = QgsVectorFileWriter("C://Users//Robbie//Desktop//", "System", vl.fields(), QGis.WKBLineString, None, "ESRI Shapefile")
-  # if writer.hasError() != QgsVectorFileWriter.NoError:
-  #   print "Error when creating shapefile: ",  writer.errorMessage()
-  # error = QgsVectorFileWriter.writeAsVectorFormat(vl, "C://Users//Robbie//Desktop//DrivingDistance.shp",
-  #                                                  "System", None, "ESRI Shapefile")
-  # if error == QgsVectorFileWriter.NoError:
-  #   print "success!"
-
-distance()
-rlayers = QgsMapLayerRegistry.instance().mapLayers().keys()
-mapRenderer.setLayerSet([vl2.id()])
-clayers = list(map(lambda clay: QgsMapCanvasLayer(clay), 
-            QgsMapLayerRegistry.instance().mapLayers().values()))
-canvas.setLayerSet([vl2_])
-rect = QgsRectangle(mapRenderer.fullExtent())
-rect.scale(1.1)
-mapRenderer.setExtent(rect)
-mapRenderer.setOutputSize(img.size(), img.logicalDpiX())
-# mapRenderer.render(img_painter)
-# sourceArea = QRectF(0, 0, c.paperWidth(), c.paperHeight())
-# targetArea = QRectF(0, 0, width, height)
-# c.render(img_painter, targetArea, sourceArea)
-# img_painter.end()
-c.exportAsPDF("Render.pdf")
-# img.save("C:\Users\Robbie\Desktop\Render.png","png")
-# c.save("C:\Users\Robbie\Desktop\Render2.png","png")
-# print(QgsMapLayerRegistry.instance().mapLayers(), len(QgsMapLayerRegistry.instance().mapLayers()))
-print("Done")
 qgs.exec_()
 qgs.exitQgis()
